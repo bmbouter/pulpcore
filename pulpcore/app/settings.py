f4e10c9526494672bba53903043f605c6defe150
Brian Bouterse <bmbouter@redhat.com>
2020-08-11 14:13:13 -0400

    Configure Pulp for ldap support
    [noissue]

--- pulpcore/app/settings.py
+++ pulpcore/app/settings.py
@@ -8,6 +8,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.11/ref/settings/
 """
 
+import ldap
 import os
 from contextlib import suppress
 from gettext import gettext as _
@@ -15,6 +16,7 @@ from importlib import import_module
 from pkg_resources import iter_entry_points
 
 from django.core.exceptions import ImproperlyConfigured
+from django_auth_ldap.config import LDAPSearch, PosixGroupType
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -101,6 +103,7 @@ MIDDLEWARE = [
 ]
 
 AUTHENTICATION_BACKENDS = [
+    "django_auth_ldap.backend.LDAPBackend",
     "django.contrib.auth.backends.ModelBackend",
     "guardian.backends.ObjectPermissionBackend",
 ]
@@ -192,6 +195,59 @@ LOGGING = {
         },
     },
 }
+
+
+#### django-auth-ldap settings ####
+
+# Baseline configuration.
+AUTH_LDAP_SERVER_URI = "ldap://localhost"
+
+AUTH_LDAP_BIND_DN = "cn=admin,dc=example,dc=org"
+AUTH_LDAP_BIND_PASSWORD = "admin"
+AUTH_LDAP_USER_SEARCH = LDAPSearch(
+    "ou=users,dc=example,dc=org", ldap.SCOPE_SUBTREE, "(uid=%(user)s)"
+)
+# Or:
+# AUTH_LDAP_USER_DN_TEMPLATE = 'uid=%(user)s,ou=users,dc=example,dc=org'
+
+# Set up the basic group parameters.
+AUTH_LDAP_GROUP_SEARCH = LDAPSearch(
+    "ou=groups,dc=example,dc=org",
+    ldap.SCOPE_SUBTREE,
+    "(objectClass=posixGroup)",
+)
+AUTH_LDAP_GROUP_TYPE = PosixGroupType(name_attr='cn')
+AUTH_LDAP_MIRROR_GROUPS = True
+
+# Simple group restrictions
+# AUTH_LDAP_REQUIRE_GROUP = "cn=enabled,ou=django,ou=groups,dc=example,dc=com"
+# AUTH_LDAP_DENY_GROUP = "cn=disabled,ou=django,ou=groups,dc=example,dc=com"
+
+# Populate the Django user from the LDAP directory.
+AUTH_LDAP_USER_ATTR_MAP = {
+    "first_name": "givenName",
+    "last_name": "sn",
+    "email": "mail",
+}
+
+# AUTH_LDAP_USER_FLAGS_BY_GROUP = {
+#     "is_active": "cn=active,ou=django,ou=groups,dc=example,dc=com",
+#     "is_staff": "cn=staff,ou=django,ou=groups,dc=example,dc=com",
+#     "is_superuser": "cn=superuser,ou=django,ou=groups,dc=example,dc=com",
+# }
+
+# This is the default, but I like to be explicit.
+AUTH_LDAP_ALWAYS_UPDATE_USER = True
+
+# Use LDAP group membership to calculate group permissions.
+AUTH_LDAP_FIND_GROUP_PERMS = True
+
+# Cache distinguished names and group memberships for an hour to minimize
+# LDAP traffic.
+AUTH_LDAP_CACHE_TIMEOUT = 1
+
+###################################
+
 
 DRF_ACCESS_POLICY = {"reusable_conditions": "pulpcore.app.global_access_conditions"}
 
